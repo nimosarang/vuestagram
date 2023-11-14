349f10a8b097c400200727eef0b74684785a184b
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-11-15 01:31:18 +0900

    finished searching Followers

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding: 10px">
     <h4>팔로워</h4>
-    <input placeholder="🔍" />
+    <input placeholder="🔍" v-model="searchFollower" @input="search" />
     <div class="post-header" v-for="(a, i) in follower" :key="i">
       <div class="profile" :style="`background-image:url(${a.image})`"></div>
       <span class="profile-name">{{ a.name }}</span>
@@ -16,14 +16,24 @@ export default {
   name: "myPage",
   setup() {
     let follower = ref([]);
+    let followerOriginal = ref([]);
+    let searchFollower = ref("");
+
+    const search = () => {
+      let newFollower = followerOriginal.value.filter((a) => {
+        return a.name.includes(searchFollower.value);
+      });
+      follower.value = [...newFollower];
+    };
 
     onMounted(() => {
       axios.get("/follower.json").then((a) => {
         follower.value = a.data;
+        followerOriginal.value = [...a.data];
       });
     });
 
-    return { follower };
+    return { follower, search, searchFollower };
   },
   data() {
     return {};
